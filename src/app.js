3748e35c4b26da85f6bf809d95a30a6e2a8d589d
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-01 18:14:09 +0000

    prettier-out

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function displayText() {
-  //write your code here
-
   let programadores = [
     {
       id: 1,
